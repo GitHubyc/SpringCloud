7a754a530652b58319bc5b4767a64c2282ece12a
GitHubyc <326831169@qq.com>
2018-02-08 15:54:28 +0800

    SpringCloud
    代码

---
+++ springcloud-customer/src/main/java/com/xakj/SpringcloudCustomerApplication.java
@@ -0,0 +1,19 @@
+package com.xakj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * 消费者（使用服务的对象）
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudCustomerApplication.class, args);
+	}
+}
